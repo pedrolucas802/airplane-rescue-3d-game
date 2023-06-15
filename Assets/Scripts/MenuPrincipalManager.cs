bcf57ea925ee357a45ee53a1da689f775c2dc23c
fabio carneiro <fabiojunior_1992@hotmail.com>
2023-06-15 15:46:36 -0300

    segundo

--- Assets/Scripts/MenuPrincipalManager.cs
+++ Assets/Scripts/MenuPrincipalManager.cs
@@ -3,21 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MenuPrincipalManager : MonoBehaviour
 {
+   [SerializeField]private string tutorial1;
+   [SerializeField]private string tutorial2;
+   [SerializeField]private GameObject painelMenu;
+   [SerializeField]private GameObject painelOpcoes;
+   [SerializeField]private GameObject painelMapa;
+
    public void Jogar(){
+    painelMenu.SetActive(false);
+    painelMapa.SetActive(true);
+   }
+
+   public void MapaAviao(){
+    SceneManager.LoadScene(tutorial1);
+    
+   }
+
+   public void MapaHelicoptero(){
+    SceneManager.LoadScene(tutorial2);
     
    }
 
    public void AbrirOpcoes(){
-
+      painelMenu.SetActive(false);
+      painelOpcoes.SetActive(true);
    }
    
-   public void SalvarOpcoes(){
+   public void VoltarOpcoes(){
+      painelMenu.SetActive(true);
+      painelOpcoes.SetActive(false);
+   }
 
+   public void VoltarJogar(){
+      painelMenu.SetActive(true);
+      painelMapa.SetActive(false);
    }
 
    public void SairJogo(){
-
+      Debug.Log("Saiu do jogo");
+      Application.Quit();
    }
 }
--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -4,8 +4,8 @@ using TMPro;
 public class ScoreSystem : MonoBehaviour
 {
     public TextMeshProUGUI scoreText; // Reference to the TextMeshProUGUI component for score display
-
     private int score = 0; // Current score
+    private bool gameEnded = false; // Flag to indicate if the game has ended
 
     public void UpdateScore()
     {
@@ -16,7 +16,7 @@ public class ScoreSystem : MonoBehaviour
         UpdateScoreText();
 
         // Check if the score reaches 5
-        if (score >= 0)
+        if (score == 5 && !gameEnded)
         {
             // Call the game over function
             GameOver();
@@ -33,7 +33,27 @@ public class ScoreSystem : MonoBehaviour
     {
         // Perform game over actions
         Debug.Log("Game Over");
-        // You can add more game over logic here, such as displaying a game over screen, stopping gameplay, etc.
+
+        // Set the gameEnded flag to true to prevent further execution of GameOver logic
+        gameEnded = true;
+
+        // Close the game
+        QuitGame();
+    }
+
+    private void QuitGame()
+    {
+        // Quit the application
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    private void Start()
+    {
+        // Start a timer to call GameOver after 2 minutes
+        Invoke("GameOver", 120f);
     }
 }
-
